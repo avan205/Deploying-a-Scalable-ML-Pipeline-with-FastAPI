6edc786e1632ef3532256b3d47f8722068320496
Amy Vander Kooi <avan205@wgu.edu>
2026-08-23 00:14:08 +0000

    Flake8 DONE

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from ml.data import apply_label, process_data
 from ml.model import inference, load_model
 
 # DO NOT MODIFY
+
+
 class Data(BaseModel):
     age: int = Field(..., example=37)
     workclass: str = Field(..., example="Private")
@@ -24,14 +26,16 @@ class Data(BaseModel):
     capital_gain: int = Field(..., example=0, alias="capital-gain")
     capital_loss: int = Field(..., example=0, alias="capital-loss")
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
-    native_country: str = Field(..., example="United-States", alias="native-country")
+    native_country: str = Field(
+        ..., example="United-States", alias="native-country"
+    )
+
 
 path = os.path.join("model", "encoder.pkl")
 encoder = load_model(path)
 
 path = os.path.join("model", "model.pkl")
 model = load_model(path)
-
 
 app = FastAPI()
 
@@ -44,12 +48,12 @@ async def get_root():
 
 @app.post("/data/")
 async def post_inference(data: Data):
-    # DO NOT MODIFY: turn the Pydantic model into a dict.
     data_dict = data.dict()
-    # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
-    # The data has names with hyphens and Python does not allow those as variable names.
-    # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
-    data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
+    data = {
+        k.replace("_", "-"): [v]
+        for k, v in data_dict.items()
+    }
+
     data = pd.DataFrame.from_dict(data)
 
     cat_features = [
@@ -64,7 +68,7 @@ async def post_inference(data: Data):
     ]
     data_processed, _, _, _ = process_data(
         data,
-        categorical_features=cat_features, 
+        categorical_features=cat_features,
         training=False,
         encoder=encoder,
     )
--- test_ml.py
+++ test_ml.py
@@ -1,10 +1,9 @@
-import pytest
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
 from ml.data import process_data
-from ml.model import compute_model_metrics, inference, train_model
+from ml.model import compute_model_metrics, train_model
 
 
 def test_one_train_model_returns_expected_algorithm_random_forest():
@@ -13,7 +12,6 @@ def test_one_train_model_returns_expected_algorithm_random_forest():
     """
     X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
     y_train = np.array([0, 1, 0, 1])
-    
     model = train_model(X_train, y_train)
 
     assert isinstance(model, RandomForestClassifier)
@@ -21,7 +19,7 @@ def test_one_train_model_returns_expected_algorithm_random_forest():
 
 def test_two_compute_model_metrics_returns_expected_values():
     """
-    # Test that compute_model_metrics returns float values between 0 and 1
+    Test that compute_model_metrics returns float values between 0 and 1
     """
     y = np.array([1, 0, 1, 1])
     preds = np.array([1, 0, 0, 1])
@@ -43,7 +41,13 @@ def test_three_process_data_returns_expected_types():
     data = pd.DataFrame(
         {
             "age": [16, 27, 38, 59, 72],
-            "race": ["White", "Asian-Pac-Islander", "Amer-Indian-Eskimo", "Other", "Black"],
+            "race": [
+                "White",
+                "Asian-Pac-Islander",
+                "Amer-Indian-Eskimo",
+                "Other",
+                "Black"
+            ],
             "sex": ["Female", "Male", "Female", "Male", "Female"],
             "salary": ["<50K", "<=50K", ">50K", ">=50K", "<50K"],
         }
@@ -52,14 +56,13 @@ def test_three_process_data_returns_expected_types():
     categorical_features = ["race", "sex"]
 
     X, y, encoder, lb = process_data(
-        data, 
+        data,
         categorical_features=categorical_features,
-        label = "salary", 
-        training = True,
+        label="salary",
+        training=True,
     )
 
     assert isinstance(X, np.ndarray)
     assert isinstance(y, np.ndarray)
     assert encoder is not None
-    assert lb is not None     
-            
+    assert lb is not None
